16a4747aee57ea404dc87553800ccba461bf7312
Al-lien <lienard.alexis@gmail.com>
2024-07-01 12:19:33 +0200

    #04 first commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# WCS Quest - Spring
-
-> This repo contains my takes on the Spring's quests. Change the branch to find the quest you're looking for.
-
-![Spring quests](./assets/screenshot.png)
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.wildcodeschool.myProject;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyProjectApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyProjectApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
