515243a5b43ad54d690f87f933016a2ce0e8fce7
Al-lien <lienard.alexis@gmail.com>
2024-07-01 16:29:28 +0200

    #05 edit docker-compose and create Article object

---
+++ src/main/java/com/example/demo/article/Article.java
@@ -0,0 +1,26 @@
+package com.example.demo.article;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "article")
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
